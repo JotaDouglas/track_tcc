a96fa951ed39da8531834d27d6854686b718e390
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-03-16 22:26:45 -0300

    feat: adição de mobx no projeto

---
+++ track_tcc_app/lib/viewmodel/login.viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:mobx/mobx.dart';
+ part 'login.viewmodel.g.dart';
+ 
+ class LoginViewModel = LoginViewModelBase
+     with _$LoginViewModel;
+ 
+ abstract class LoginViewModelBase with Store {
+   
+ }
