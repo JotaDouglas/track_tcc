c4a5db5af4c67398c2466a1a81c393063c238adc
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-04-28 22:04:43 -0300

    feat: adição de imagem em screen map

--- lib/views/track/tracking.view.dart
+++ lib/views/track/tracking.view.dart
@@ -2,12 +2,14 @@ import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:track_tcc_app/helper/location.helper.dart';
 import 'package:track_tcc_app/model/place.model.dart';
 import 'package:track_tcc_app/viewmodel/tracking.viewmodel.dart';
 import 'package:track_tcc_app/views/map.view.dart';
+import 'package:track_tcc_app/views/widgets/loading.widget.dart';
 
 class TrackPage extends StatefulWidget {
   const TrackPage({super.key});
@@ -61,14 +63,19 @@ class _TrackPageState extends State<TrackPage> {
     }
   }
 
+  void popWidget() {
+    Navigator.pop(context);
+  }
+
   Future<void> startTrack() async {
+    if (!loopOn) Dialogs.showLoading(context, GlobalKey());
     toggleTrackingState();
 
     if (loopOn) {
       final newLocal = await _locationHelper.actuallyPosition();
-
       if (newLocal != null) {
         await viewModel.startTracking(newLocal); // Insere a nova rota
+        // ignore: use_build_context_synchronously
         listMap.add(LatLng(newLocal.latitude!, newLocal.longitude!));
         setState(() {
           trackList.insert(0, newLocal);
@@ -80,6 +87,9 @@ class _TrackPageState extends State<TrackPage> {
     } else {
       if (trackList.isNotEmpty) {
         await viewModel.stopTracking(trackList.first); // Finaliza a rota
+        setState(() {
+          trackList.clear();
+        });
       }
       stopTracking(); // Cancela o timer
     }
@@ -98,6 +108,7 @@ class _TrackPageState extends State<TrackPage> {
 
   Future<void> getCurrentLocation() async {
     log('Iniciando loop de rastreamento...');
+    popWidget();
 
     temp = Timer.periodic(
       const Duration(seconds: 5),
@@ -135,6 +146,7 @@ class _TrackPageState extends State<TrackPage> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -154,8 +166,31 @@ class _TrackPageState extends State<TrackPage> {
             children: [
               trackList.isEmpty
                   ? Expanded(
-                      child:
-                          const Center(child: Text("Nenhum local registrado.")))
+                      child: Center(
+                          child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/newTracking.svg',
+                          width: size.width * 0.3,
+                          height: size.height * 0.3,
+                          fit: BoxFit.contain,
+                          semanticsLabel: 'Nova ilustração de tracking',
+                          colorFilter: ColorFilter.mode(
+                            Colors.orange[800] ??
+                                Colors.orange, // cor que você quiser
+                            BlendMode.srcIn, // mantém a forma do SVG
+                          ),
+                        ),
+                        const Text(
+                          "Inicie um novo rastreamento.",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )))
                   : Expanded(
                       child: TrackingMapWidget(
                         trackList: listMap,
