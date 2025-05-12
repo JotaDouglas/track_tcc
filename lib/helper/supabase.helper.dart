e12162bf1fb5f2129017777330256c789e10b14e
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-05-12 19:43:55 -0300

    link com novo banco supabase

--- lib/helper/supabase.helper.dart
+++ lib/helper/supabase.helper.dart
@@ -1,5 +1,5 @@
 class SupabaseLogin {
-  String supabaseUrl = "https://vrquhcakodbguxbbnewm.supabase.co";
+  String supabaseUrl = "https://ucxtuobxnlzrusyajqzc.supabase.co";
   String supabaseKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZycXVoY2Frb2RiZ3V4YmJuZXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NTc0ODcsImV4cCI6MjA2MjMzMzQ4N30.NUFbd4PjnAjys-pw5VsYd_3rGepvrkviixu3_So1vPs";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjeHR1b2J4bmx6cnVzeWFqcXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjMwMjYsImV4cCI6MjA2MjYzOTAyNn0.x_EU0TOcppugZaJzI4BeXDVYFRuPEd0-jcdijEOPqDo";
 }
