b8eb9774532d8ca320d12127e278ebd0e3c79328
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-06-02 17:29:53 -0300

    feat: Adição de validações em signup

--- lib/views/login/signup.view.dart
+++ lib/views/login/signup.view.dart
@@ -43,11 +43,14 @@ class CadastroViewState extends State<CadastroView> {
     if (value == null || value.isEmpty) {
       return 'O e-mail é obrigatório';
     }
-    final emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+
+    final emailRegex =
+        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 
     if (!emailRegex.hasMatch(value)) {
       return 'Digite um e-mail válido';
     }
+
     return null;
   }
 
@@ -55,9 +58,18 @@ class CadastroViewState extends State<CadastroView> {
     if (value == null || value.isEmpty) {
       return 'A senha é obrigatória';
     }
-    if (value.length < 6) {
-      return 'A senha deve ter pelo menos 6 caracteres';
+
+    if (value.length < 6 || value.length > 12) {
+      return 'A senha deve ter entre 6 e 12 caracteres';
     }
+
+    final hasLetters = RegExp(r'[a-zA-Z]').hasMatch(value);
+    final hasNumbers = RegExp(r'\d').hasMatch(value);
+
+    if (!(hasLetters && hasNumbers)) {
+      return 'A senha deve conter letras e números';
+    }
+
     return null;
   }
 
