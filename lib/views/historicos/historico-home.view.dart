0a8907da4409a862afd300f1059813f952425b7c
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-07-24 22:24:33 -0300

    feat: Criação da tela de Sincronizadas e Não sincronizadas

--- lib/views/historicos/historico-home.view.dart
+++ lib/views/historicos/historico-home.view.dart
@@ -11,9 +11,10 @@ class RotasPage extends StatefulWidget {
   State<RotasPage> createState() => _RotasPageState();
 }
 
-class _RotasPageState extends State<RotasPage> {
+class _RotasPageState extends State<RotasPage> with TickerProviderStateMixin {
   final TrackingViewModel viewModel = TrackingViewModel();
   List<PlaceModel> rotas = [];
+  late TabController tabController;
 
   @override
   void initState() {
@@ -55,128 +56,188 @@ class _RotasPageState extends State<RotasPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Minhas Rotas',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+    tabController = TabController(length: 2, vsync: this);
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Minhas Rotas',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.orange[900],
+          foregroundColor: Colors.white,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.orange[900],
-        foregroundColor: Colors.white,
-      ),
-      body: rotas.isEmpty
-          ? const Center(child: Text('Nenhuma rota encontrada.'))
-          : ListView.builder(
-              itemCount: rotas.length,
-              itemBuilder: (context, index) {
-                final rota = rotas[index];
-                return Card(
-                  elevation: 3,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    title: Text(
-                      "🚩 Rota ${index + 1}",
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w600, fontSize: 16),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body: Column(
+          children: [
+            TabBar(
+              controller: tabController,
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(icon: Text("Não Sincronizadas")),
+                Tab(icon: Text("Sincronizadas")),
+              ],
+              onTap: (value) {},
+            ),
+            Expanded(
+              child: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                controller: tabController,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        bottom: 16, left: 16, right: 16, top: 10),
+                    child: Column(
                       children: [
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            const Icon(Icons.play_arrow,
-                                size: 16, color: Colors.green),
-                            const SizedBox(width: 4),
-                            Text(
-                              DateConversion.convertDateTimeFromString(
-                                  rota.dateInicial!),
-                              style: const TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 4),
-                        Row(
-                          children: [
-                            const Icon(Icons.stop, size: 16, color: Colors.red),
-                            const SizedBox(width: 4),
-                            Text(
-                              DateConversion.convertDateTimeFromString(
-                                  rota.dateFinal ?? ''),
-                              style: const TextStyle(fontSize: 14),
-                            ),
-                          ],
+                        Expanded(
+                          child: rotas.isEmpty
+                              ? const Center(
+                                  child: Text('Nenhuma rota encontrada.'))
+                              : ListView.builder(
+                                  itemCount: rotas.length,
+                                  itemBuilder: (context, index) {
+                                    final rota = rotas[index];
+                                    return Card(
+                                      elevation: 3,
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 16, vertical: 8),
+                                      child: ListTile(
+                                        title: Text(
+                                          "🚩 Rota ${index + 1}",
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 16),
+                                        ),
+                                        subtitle: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            const SizedBox(height: 8),
+                                            Row(
+                                              children: [
+                                                const Icon(Icons.play_arrow,
+                                                    size: 16,
+                                                    color: Colors.green),
+                                                const SizedBox(width: 4),
+                                                Text(
+                                                  DateConversion
+                                                      .convertDateTimeFromString(
+                                                          rota.dateInicial!),
+                                                  style: const TextStyle(
+                                                      fontSize: 14),
+                                                ),
+                                              ],
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Row(
+                                              children: [
+                                                const Icon(Icons.stop,
+                                                    size: 16,
+                                                    color: Colors.red),
+                                                const SizedBox(width: 4),
+                                                Text(
+                                                  DateConversion
+                                                      .convertDateTimeFromString(
+                                                          rota.dateFinal ?? ''),
+                                                  style: const TextStyle(
+                                                      fontSize: 14),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                        trailing: PopupMenuButton<String>(
+                                          icon: const Icon(Icons.more_vert),
+                                          onSelected: (value) async {
+                                            switch (value) {
+                                              case 'mapa':
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (_) =>
+                                                        HistoricMapFlutter(
+                                                            IdTrack: rota.id!),
+                                                  ),
+                                                );
+                                                break;
+                                              case 'sync':
+                                                await viewModel.syncRota(rota);
+                                                break;
+                                              case 'delete':
+                                                deletarRota(rota.id!);
+                                                break;
+                                            }
+                                          },
+                                          itemBuilder: (context) {
+                                            final List<PopupMenuEntry<String>>
+                                                items = [
+                                              const PopupMenuItem(
+                                                value: 'mapa',
+                                                child: ListTile(
+                                                  leading: Icon(Icons.map,
+                                                      color: Colors.blue),
+                                                  title: Text('Ver no mapa'),
+                                                ),
+                                              ),
+                                            ];
+
+                                            // Só adiciona a opção de sincronizar se ainda não foi sincronizado
+                                            if (rota.idSistema == null) {
+                                              items.add(
+                                                const PopupMenuItem(
+                                                  value: 'sync',
+                                                  child: ListTile(
+                                                    leading: Icon(Icons.sync,
+                                                        color: Colors.orange),
+                                                    title: Text('Sincronizar'),
+                                                  ),
+                                                ),
+                                              );
+                                            }
+
+                                            // Sempre mostra excluir
+                                            items.add(
+                                              const PopupMenuItem(
+                                                value: 'delete',
+                                                child: ListTile(
+                                                  leading: Icon(Icons.delete,
+                                                      color: Colors.red),
+                                                  title: Text('Excluir'),
+                                                ),
+                                              ),
+                                            );
+
+                                            return items;
+                                          },
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
                         ),
                       ],
                     ),
-                    trailing: PopupMenuButton<String>(
-                      icon: const Icon(Icons.more_vert),
-                      onSelected: (value) async {
-                        switch (value) {
-                          case 'mapa':
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) =>
-                                    HistoricMapFlutter(IdTrack: rota.id!),
-                              ),
-                            );
-                            break;
-                          case 'sync':
-                            await viewModel.syncRota(rota);
-                            break;
-                          case 'delete':
-                            deletarRota(rota.id!);
-                            break;
-                        }
-                      },
-                      itemBuilder: (context) {
-                        final List<PopupMenuEntry<String>> items = [
-                          const PopupMenuItem(
-                            value: 'mapa',
-                            child: ListTile(
-                              leading: Icon(Icons.map, color: Colors.blue),
-                              title: Text('Ver no mapa'),
-                            ),
-                          ),
-                        ];
-
-                        // Só adiciona a opção de sincronizar se ainda não foi sincronizado
-                        if (rota.idSistema == null) {
-                          items.add(
-                            const PopupMenuItem(
-                              value: 'sync',
-                              child: ListTile(
-                                leading: Icon(Icons.sync, color: Colors.orange),
-                                title: Text('Sincronizar'),
-                              ),
-                            ),
-                          );
-                        }
-
-                        // Sempre mostra excluir
-                        items.add(
-                          const PopupMenuItem(
-                            value: 'delete',
-                            child: ListTile(
-                              leading: Icon(Icons.delete, color: Colors.red),
-                              title: Text('Excluir'),
-                            ),
-                          ),
-                        );
-
-                        return items;
-                      },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Text("text"),
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
+                ],
+              ),
             ),
+          ],
+        ),
+      ),
     );
   }
 }
