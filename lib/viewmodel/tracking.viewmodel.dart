c94e1980e9c6b627979171942b66da71d90e18ca
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-08-20 06:44:22 -0300

    feat: adição de dados relacionados ao processo de tracking na viewmodel

--- lib/viewmodel/tracking.viewmodel.dart
+++ lib/viewmodel/tracking.viewmodel.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:latlong2/latlong.dart';
 import 'package:mobx/mobx.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:track_tcc_app/helper/location.helper.dart';
 import 'package:track_tcc_app/model/place.model.dart';
 import 'package:track_tcc_app/repository/track.repository.dart';
+import 'package:track_tcc_app/viewmodel/login.viewmodel.dart';
 
 part 'tracking.viewmodel.g.dart';
 
@@ -13,6 +17,8 @@ class TrackingViewModel = TrackingViewModelBase with _$TrackingViewModel;
 abstract class TrackingViewModelBase with Store {
   final TrackRepository trackRepository = TrackRepository();
   final SupabaseClient _supabase = Supabase.instance.client;
+  final Locationhelper _locationHelper = Locationhelper();
+  final LoginViewModel authViewModel = LoginViewModel();
 
   int? currentRotaId;
 
@@ -27,6 +33,41 @@ abstract class TrackingViewModelBase with Store {
 
   @observable
   bool loading = false;
+
+  //Sistema de track
+
+  @observable
+  bool trackingLoop = false;
+
+  @observable
+  bool trackingMode = false;
+
+  @observable
+  double distanceMeters = 0.0;
+
+  @observable
+  PlaceModel? lastPlace;
+
+  @observable
+  LatLng? lastPosition;
+
+  @observable
+  String addressLabel = '';
+
+  @observable
+  List<PlaceModel> trackListLoop = [];
+
+  @observable
+  Timer? temp;
+
+  @action
+  changeDistance(double value, {bool reset = false}) {
+    distanceMeters += value;
+
+    if (reset) {
+      distanceMeters = 0.0;
+    }
+  }
 
   @action
   changeLoading(bool value) => loading = value;
@@ -110,7 +151,7 @@ abstract class TrackingViewModelBase with Store {
       } catch (e) {
         log("erro ao sincronizar automaticamente");
       }
-      
+
       currentRotaId = null;
     } catch (e) {
       log("Erro ao parar rastreamento: $e");
@@ -227,5 +268,100 @@ abstract class TrackingViewModelBase with Store {
     getRotasOnline();
 
     changeLoading(false);
+  }
+
+  //Sistema de tracking
+  Future<void> _startSharing() async {
+    //Sinal GPS
+    bool gpsOn;
+    gpsOn = await Locationhelper().checkGps(context);
+    if (gpsOn != true) return;
+
+    toggleTrackingState();
+
+    trackingMode = trackingLoop;
+    changeDistance(0, reset: true);
+
+    if (trackingLoop) {
+      final newLocal = await _locationHelper.actuallyPosition();
+
+      if (newLocal != null) {
+        if (currentRotaId == null) {
+          await insertTracking(newLocal);
+        }
+
+        lastPlace = newLocal;
+        lastPosition =
+            LatLng(newLocal.latitude ?? 0.0, newLocal.longitude ?? 0.0);
+        addressLabel = newLocal.adress ?? 'Endereço não encontrado';
+
+        trackListLoop.insert(0, newLocal);
+
+        primeiroTrack(); // faz a primeira leitura
+        _startTimer(); // começa o timer para continuar rastreando
+      } else {
+        toggleTrackingState();
+        trackingLoop = false;
+      }
+    } else {
+      if (trackListLoop.isNotEmpty) {
+        await stopTracking(trackListLoop.first);
+      }
+
+      _stopSharing();
+      trackListLoop.clear();
+      addressLabel = '';
+      lastPlace = null;
+      lastPosition = null;
+      distanceMeters = 0.0;
+    }
+  }
+
+  toggleTrackingState() {
+    trackingLoop = !trackingLoop;
+  }
+
+  Future<void> primeiroTrack() async {
+    try {
+      final newLocal = await _locationHelper.actuallyPosition();
+
+      if (newLocal != null) {
+        // Calcular distância
+        final newLatLng =
+            LatLng(newLocal.latitude ?? 0.0, newLocal.longitude ?? 0.0);
+        if (lastPosition != null) {
+          distanceMeters += const Distance().as(
+            LengthUnit.Meter,
+            lastPosition!,
+            newLatLng,
+          );
+        }
+
+        lastPosition = newLatLng;
+        lastPlace = newLocal;
+        addressLabel = newLocal.adress ?? 'Endereço não encontrado';
+
+        await trackLocation(newLocal, authViewModel.loginUser?.username  ?? 'Sem nome');
+
+        trackListLoop.insert(0, newLocal);
+      } else {
+        log('Localização retornou null.');
+      }
+    } catch (e) {
+      log('Erro no rastreamento: $e');
+      _stopSharing();
+      toggleTrackingState();
+    }
+  }
+
+  void _startTimer() async {
+    temp = Timer.periodic(const Duration(seconds: 5), (_) => primeiroTrack());
+  }
+
+  void _stopSharing() async {
+    temp?.cancel();
+    temp = null;
+    log('Rastreamento finalizado');
+    trackingMode = false;
   }
 }
