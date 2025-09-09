4c2f3adca882ad37e42d3d0a17107d40d90bf8ab
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-09-08 21:31:28 -0300

    feat: geofence viewmodel

---
+++ lib/viewmodel/geofence.viewmodel.dart
@@ -0,0 +1,21 @@
+import 'package:mobx/mobx.dart';
+import 'package:latlong2/latlong.dart';
+
+part 'geofence.viewmodel.g.dart';
+
+class GeofenceStore = _GeofenceStoreBase with _$GeofenceStore;
+
+abstract class _GeofenceStoreBase with Store {
+  @observable
+  ObservableList<LatLng> pontos = ObservableList<LatLng>();
+
+  @action
+  void adicionarPonto(LatLng ponto) {
+    pontos.add(ponto);
+  }
+
+  @action
+  void limparPontos() {
+    pontos.clear();
+  }
+}
