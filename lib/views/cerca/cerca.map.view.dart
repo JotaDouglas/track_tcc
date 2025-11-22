6f726772d124e19b0d09fbe286c0305fcaceac4b
João Douglas Silva dos Santos <133287471+JotaDouglas@users.noreply.github.com>
2025-11-22 14:11:54 -0300

    fix: Correção de acesso ao mapa de rotas

--- lib/views/cerca/cerca.map.view.dart
+++ lib/views/cerca/cerca.map.view.dart
@@ -104,9 +104,11 @@ class _CercaMapViewState extends State<CercaMapView> {
               children: [
                 TileLayer(
                   urlTemplate:
-                      'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-                  subdomains: const ['a', 'b', 'c'],
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                  userAgentPackageName: 'com.tracktcc.nome',
                 ),
+
                 // Polígonos das cercas
                 ..._buildPolygons(vm),
                 // Marcadores (pins)
